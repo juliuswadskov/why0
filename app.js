21427a2330d7cae3da6d88ec42e8aa200235d9d5
Raag2005 <oskar@raagart.dk>
2021-03-27 12:20:26 +0100

    update

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const fs = require('fs');
 
-why0 = (cb)=>{const files = fs.readdirSync('./why0/').filter(file => file.endsWith('.why0'));for (const file of files) {fs.readFile(`./why0/${file}`, 'utf8', (err, data) => {if (err) {console.log(err);return} else {cb(data)}})}}
+why0 = (cb) => {  
+    const files = fs.readdirSync('./why0/').filter(file => file.endsWith('.why0'));
+    for (const file of files) {
+        fs.readFile(`./why0/${file}`, 'utf8', (err, data) => {
+            if (err) {
+                console.log(err);
+                return
+            } else {
+                cb(data)
+            }
+        })
+    }
+}
 
 why0((data) => {
     console.log(data)
